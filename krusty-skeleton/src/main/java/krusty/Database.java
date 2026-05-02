f2689edab868ac81e294a5fd42323a5470b70b76
caspian <caspian@roesengren.nu>
2026-05-02 13:57:59 +0200

    ny kan man skapa en palet, men den updaterar inte inveteringen

--- krusty-skeleton/src/main/java/krusty/Database.java
+++ krusty-skeleton/src/main/java/krusty/Database.java
@@ -9,6 +9,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import org.checkerframework.checker.units.qual.N;
+
 import spark.Request;
 import spark.Response;
 
@@ -128,7 +130,57 @@ public class Database {
 	}
 
 	public String createPallet(Request req, Response res) {
-		return "{}";
+		
+
+
+		String lägTillPall = "INSERT INTO Pallets (cookie, production_date, blocked) VALUES (?, LOCALTIMESTAMP, ?)";
+		String ValdKacka = req.queryParams("cookie");
+		int paletId;
+		if(ValdKacka == null){
+			return "{\"status\": \"error\"}";
+		}
+
+		try{
+			kont.setAutoCommit(false);
+			String SeOmKakaFins = "SELECT name FROM products WHERE name = ?";
+			try(PreparedStatement pstmt = kont.prepareStatement(SeOmKakaFins)){
+				pstmt.setString(1, ValdKacka);
+				ResultSet r = pstmt.executeQuery();
+				if (!r.next()) {
+					kont.setAutoCommit(true);
+					return "{\"status\": \"unknown cookie\"}";
+				}
+			}
+			try(PreparedStatement sKapaPaler = kont.prepareStatement(lägTillPall,java.sql.Statement.RETURN_GENERATED_KEYS)){
+				sKapaPaler.setString(1, ValdKacka);
+				sKapaPaler.setString(2, "no");
+				sKapaPaler.executeUpdate();
+				ResultSet Nyklar = sKapaPaler.getGeneratedKeys();
+				Nyklar.next();
+				paletId = Nyklar.getInt(1);
+			}
+			
+
+
+
+
+
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+			try {
+				kont.rollback();
+				kont.setAutoCommit(true); 
+			} 
+			catch (SQLException FellFell) {
+				FellFell.printStackTrace();
+			}
+			return "{\"status\": \"error\"}";
+		}
+
+
+
+return "{}";
 	}
 	private String GetDatta(String fråga, Connection kont, String Destinaton){
 		try (
